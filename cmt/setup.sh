e369d6e4d376422bbc66c74e65d08fd3fe012219
focke <>
2009-07-10 20:25:28 +0000

    fileOps changes mostly done

--- cmt/setup.sh
+++ cmt/setup.sh
@@ -1,4 +1,4 @@
-# echo "Setting GPLtools v1r13 in /a/surrey01/vol/vol2/g.svac/focke/L1"
+# echo "Setting fileOps  in /afs/slac.stanford.edu/g/glast/ground/releases/volume01/L1Proc/test/1.74/GPLtools/b/GPLtools"
 
 if test "${CMTROOT}" = ""; then
   CMTROOT=/afs/slac/g/glast/applications/CMT/v1r18p20061003; export CMTROOT
@@ -7,6 +7,6 @@ fi
 
 tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
 if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt setup -sh -pack=GPLtools -version=v1r13 -path=/a/surrey01/vol/vol2/g.svac/focke/L1  -no_cleanup $* >${tempfile}; . ${tempfile}
+${CMTROOT}/mgr/cmt setup -sh -pack=fileOps -version= -path=/afs/slac.stanford.edu/g/glast/ground/releases/volume01/L1Proc/test/1.74/GPLtools/b/GPLtools  -no_cleanup $* >${tempfile}; . ${tempfile}
 /bin/rm -f ${tempfile}
 
